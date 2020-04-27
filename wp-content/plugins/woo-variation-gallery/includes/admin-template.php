aee3ad8753217a75193944d2ec1f8d85bd27abea
sushen <sushenbiswasaga@gmail.com>
2020-04-27 08:46:52 +0600

    Initial commit

---
+++ wp-content/plugins/woo-variation-gallery/includes/admin-template.php
@@ -0,0 +1,19 @@
+<?php
+	defined( 'ABSPATH' ) or die( 'Keep Quit' );
+	
+	
+	
+	//print_r( $gallery_images);
+	
+	foreach ( $gallery_images as $image_id ):
+		
+		$image = wp_get_attachment_image_src( $image_id );
+		
+		?>
+        <li class="image">
+            <input type="hidden" name="woo_variation_gallery[<?php echo esc_attr( $variation_id ) ?>][]" value="<?php echo $image_id ?>">
+            <img src="<?php echo esc_url( $image[ 0 ] ) ?>">
+            <a href="#" class="delete remove-woo-variation-gallery-image"><span class="dashicons dashicons-dismiss"></span></a>
+        </li>
+	
+	<?php endforeach; ?>
